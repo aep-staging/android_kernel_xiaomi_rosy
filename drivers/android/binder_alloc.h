533684e4b5727a508a0a3a9ca577c81644f9b809
Todd Kjos <tkjos@google.com>
2020-11-20 15:37:43 -0800

    UPSTREAM: binder: add flag to clear buffer on txn complete
    Add a per-transaction flag to indicate that the buffer
    must be cleared when the transaction is complete to
    prevent copies of sensitive data from being preserved
    in memory.
    
    Signed-off-by: Todd Kjos <tkjos@google.com>
    Link: https://lore.kernel.org/r/20201120233743.3617529-1-tkjos@google.com
    Cc: stable <stable@vger.kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Bug: 171501513
    Change-Id: Ic9338c85cbe3b11ab6f2bda55dce9964bb48447a
    (cherry picked from commit 0f966cba95c78029f491b433ea95ff38f414a761)
    Signed-off-by: Todd Kjos <tkjos@google.com>
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: azrim <mirzaspc@gmail.com>

--- drivers/android/binder_alloc.h
+++ drivers/android/binder_alloc.h
@@ -61,8 +61,7 @@ struct binder_buffer {
 	unsigned clear_on_free:1;
 	unsigned allow_user_free:1;
 	unsigned async_transaction:1;
-	unsigned oneway_spam_suspect:1;
-	unsigned debug_id:27;
+	unsigned debug_id:28;
 
 	struct binder_transaction *transaction;
 
