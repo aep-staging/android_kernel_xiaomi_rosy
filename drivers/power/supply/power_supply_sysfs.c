9aba84624c34b0861fc9820e2dd15b443a21f68d
baunilla <baunilla@tuta.io>
2021-02-08 20:18:55 +0000

    drivers: Import Xiaomi power-supply changes
    * Adapted from MiCode/Xiaomi_Kernel_OpenSource@dbfcf25 for msm-4.9

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -314,7 +314,6 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(hw_current_max),
 	POWER_SUPPLY_ATTR(real_type),
 	POWER_SUPPLY_ATTR(charging_call_state),
-	POWER_SUPPLY_ATTR(batfet_reset_enable),
 	POWER_SUPPLY_ATTR(pr_swap),
 	POWER_SUPPLY_ATTR(cc_step),
 	POWER_SUPPLY_ATTR(cc_step_sel),
